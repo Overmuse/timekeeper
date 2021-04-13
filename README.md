f4cfaa24256557d841904f3d69428e410a1b5bfd
Sebastian Rollen <seb@overmu.se>
2021-04-13 13:01:54 -0400

    committing initial application

---
+++ README.md
@@ -0,0 +1,2 @@
+# timekeeper
+`timekeeper` is a simple app designed to be run as a cron job. On each invocation, `timekeeper` reports the current market state and time to next market state transition to the kafka `time` topic.
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use alpaca::{
+    rest::clock::{Clock, GetClock},
+    Client,
+};
+use anyhow::{anyhow, Result};
+use dotenv::dotenv;
+use rdkafka::producer::FutureRecord;
+use serde::{Deserialize, Serialize};
+
+mod settings;
+use settings::Settings;
+
+#[derive(Deserialize, Serialize)]
+#[serde(tag = "state", rename_all = "lowercase")]
+pub enum State {
+    Open { next_close: usize },
+    Closed { next_open: usize },
+}
+
+impl From<Clock> for State {
+    fn from(clock: Clock) -> State {
+        if clock.is_open {
+            State::Open {
+                next_close: (clock.next_close - clock.timestamp).num_seconds() as usize,
+            }
+        } else {
+            State::Closed {
+                next_open: (clock.next_open - clock.timestamp).num_seconds() as usize,
+            }
+        }
+    }
+}
+
+async fn run() -> Result<()> {
+    let _ = dotenv();
+    let settings = Settings::new()?;
+    let producer = kafka_settings::producer(&settings.kafka)?;
+    let client = Client::new(
+        settings.alpaca.base_url,
+        settings.alpaca.key_id,
+        settings.alpaca.secret_key,
+    )?;
+
+    let clock = client.send(GetClock).await?;
+    let state: State = clock.into();
+    producer
+        .send(
+            FutureRecord::to("time")
+                .key("time")
+                .payload(&serde_json::to_string(&state)?),
+            None,
+        )
+        .await
+        .map(|_| ())
+        .map_err(|(e, msg)| anyhow!("Error from Kafka: {:?}\n Message: {:?}", e, msg))
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    match run().await {
+        Ok(_) => std::process::exit(0),
+        Err(e) => {
+            eprintln!("{:?}", e);
+            std::process::exit(1)
+        }
+    }
+}
